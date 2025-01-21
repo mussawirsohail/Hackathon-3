de5f97c459ae651829938ae4672c8b685c3972bd
Mussawir Sohail <159765711+mussawirsohail@users.noreply.github.com>
2025-01-21 22:00:56 +0500

    Delete sanity directory

--- sanity/lib/imageLoader.ts
+++
@@ -1,5 +0,0 @@
-export default function sanityImageLoader({ src, width, quality }: { src: string, width: number, quality?: number }) {
-    return `${src}?w=${width}&q=${quality || 75}`
-  }
-  
-  
